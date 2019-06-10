30ebc4718436e2604a58282e6817477213723af8
Phil <phil@phildev.168.0.111>
2019-06-11 00:39:08 +0300

    Initialized DOM repo

---
+++ dom.js
@@ -0,0 +1,17 @@
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text-content!';
+var para=document.createElement('p');
+var head_3=document.createElement('h3');
+head_3.textContent='I\'m a blue h3';
+head_3.style.cssText='color:blue;';
+container.appendChild(content);
+var div=document.createElement('div').append(head_1, para2);
+pink.style='borderColor:black; backgroundColor:pink;'
+var head_1=document.createElement('h1');
+var para2=document.createElement('p');
+para2.textContent='ME TOO!';
+
+
